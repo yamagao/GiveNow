955b427e03e1022f20cffbf10da0f05ea798b6b4
yamagao <yamagao@126.com>
2015-06-11 13:29:26 -0400

    using variables

---
+++ full_width_landing.php
@@ -0,0 +1,14 @@
+<!--Left Column-->
+<div id="content-left" class="medium-12 columns">
+  <t4 type="meta" meta="html_anchor" />
+  
+  	<h1><t4 type="content" name="Main Page Heading" output="normal" modifiers="striptags"  /></h1>
+  	<t4 type="content" name="Main Page Body" output="normal" modifiers="medialibrary, nav_sections"  />
+<?php 
+$GLOBALS['<t4 type="content" name="Name" output="normal" modifiers=""  />'] = '<t4 type="content" name="List of Funds" output="normal" modifiers=""  />';
+echo $GLOBALS['<t4 type="content" name="Name" output="normal" modifiers=""  />'];
+?>
+ 	<p class="last-modified">Last Modified:&nbsp;<i><t4 type="meta" meta="last_modified" /></i></p>
+  </div>
+    
+    <!--End Left Column-->
